5a6621510321e1efe89788e05c8180874505cbe7
Diazssj17 <Diazssj17@users.noreply.github.com>
2026-08-20 13:19:14 -0500

    Arregla campo de categoria: el boton aplastaba el input

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Permite que la app funcione sin conexión y se pueda instalar en el celular.
  */
 
-const CACHE_NOMBRE = "konta-v17";
+const CACHE_NOMBRE = "konta-v18";
 const ARCHIVOS_PARA_CACHE = [
   "./",
   "./index.html",
